93387f481f4d3c4a326f9a31feacf09375d64107
sasha74 <rolliks@gmail.com>
2023-09-30 08:07:35 -0400

    consot.log

--- src/controllers/caseController.ts
+++ src/controllers/caseController.ts
@@ -1,32 +1,35 @@
-import { loadDB } from '../modules/db.js'; 
+import { loadDB } from '../modules/db.js';
 import { Request, Response } from 'express';
 import { splitString } from '../helpers/spliProc.js';
 import { allThemes, insertThemes, deleteThemes } from './themes.route.js';
 import { allPosts, insertPosts, deletePosts } from './posts.route.js';
 
-export function getDBRoute() {return async (req: Request, res:Response) => {
-    const prm = req.body.prm;
-    const [qcolection, qaction] = splitString(req.body.procedureName);
+export function getDBRoute() {
+    return async (req: Request, res: Response) => {
+        const prm = req.body.prm;
+        console.log(req.body);
+        const [qcolection, qaction] = splitString(req.body.procedureName);
         let db = await loadDB();
         if (qcolection === 'themes') {
             if (qaction === 's') {
-                await allThemes (req, res);
-             } 
+                await allThemes(req, res);
+            }
             else if (qaction === 'iu') {
-                await insertThemes (req, res);
+                await insertThemes(req, res);
             }
             else if (qaction === 'd') {
-                await deleteThemes (req, res);
+                await deleteThemes(req, res);
             }
         } else if (qcolection === 'posts') {
             if (qaction === 's') {
-                await allPosts (req, res);
-             } 
+                await allPosts(req, res);
+            }
             else if (qaction === 'iu') {
-                await insertPosts (req, res);
+                await insertPosts(req, res);
             }
             else if (qaction === 'd') {
-                await deletePosts (req, res);
+                await deletePosts(req, res);
             }
         }
-}}
+    }
+}
