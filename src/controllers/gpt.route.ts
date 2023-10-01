d3afba014f9c203b76fc68ee96b44f631325d2aa
Makarenko Aleksandr <rolliks@gmail.com>
2023-10-01 21:10:13 +0500

    system_msg

--- src/controllers/gpt.route.ts
+++ src/controllers/gpt.route.ts
@@ -50,7 +50,8 @@ export const getGPTChat = async (req:Request, res:Response) => {
         });
 
         if (response.data) {
-            //запишем в базу ответ интелекта
+            //запишем в базу ответ интелекта 
+            /*
             console.log('======new_post========');
             console.log(response.data.choices[0]?.message?.content);
             console.log( JSON.stringify(response.data.choices[0]?.message?.content));
@@ -60,6 +61,7 @@ export const getGPTChat = async (req:Request, res:Response) => {
                 asystens_msg: JSON.stringify(response.data.choices[0]?.message?.content)
             };
             const uitems = await Post_IU(new_post);
+            */
             //теперь бы уменьшить размер ответа, чтобы экономить токены.
             //доверим это интелекту
 
