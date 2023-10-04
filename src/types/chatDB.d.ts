7abe5a4e8b08d61e9a525563df260556a0b216bd
Makarenko Aleksandr <rolliks@gmail.com>
2023-10-04 22:21:04 +0500

    add Token count

--- src/types/chatDB.d.ts
+++ src/types/chatDB.d.ts
@@ -1,22 +1,27 @@
-import { ObjectId } from 'mongodb';
+import { ObjectId } from "mongodb";
 
 export type Tthemes = {
-    _id: ObjectId,
-    p_id: ObjectId | null,
-    theme_title?: string,
-    system_msg?: string,
-    created_at?: Date,
-    updated_at?: Date | undefined,
-}
+  _id: ObjectId;
+  p_id: ObjectId | null;
+  theme_title?: string;
+  system_msg?: string;
+  created_at?: Date;
+  updated_at?: Date | undefined;
+};
 
 export type Tposts = {
-    _id: ObjectId,
-    theme_id: ObjectId,
-    user_msg: string,
-    assistant_msg?: string,
-    assistant_short_msg?: string,
-    created_at?: Date,
-    updated_at?: Date,
-}
+  _id: ObjectId;
+  theme_id: ObjectId;
+  user_msg: string;
+  assistant_msg?: string;
+  assistant_short_msg?: string;
+  created_at?: Date;
+  updated_at?: Date;
+  model?: string;
+  usage?: {
+    prompt_tokens: number;
+    completion_tokens: number;
+    total_tokens: number;
+  };
+};
 
-//assistant to assistant
